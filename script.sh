d14266e967033284e5d373b5fb4d8f3c7a1173b0
Gregor MacLennan <gmaclennan@awana.digital>
2025-03-14 13:39:52 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,9 +3,13 @@
 # Fail the job if there are any failures below.
 set -e
 
-test -f "$2" || (echo "Error! Expected a package-lock.json file but none was found." && exit 1)
+# Check if the file exists
+if [ ! -f "$2" ]; then
+    echo "Error! Expected a package-lock.json file at '$2' but none was found."
+    exit 1
+fi
 
-VER=`cat "$2" | jq '.lockfileVersion'`
+VER=$(jq '.lockfileVersion' "$2")
 
 if [ "$1" != "$VER" ]; then
     echo "Error! package-lock.json lockfileVersion is $VER but expected $1" && exit 1
