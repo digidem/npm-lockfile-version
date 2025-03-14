c42a24936edf4bdab05f57ff1f07781375a14f5c
Gregor MacLennan <gmaclennan@awana.digital>
2025-03-14 14:59:00 +0900

    feat: Add path option (#1)
    * feat: Add path input
    
    * Use path input to check lockfile
    
    * Update tests.yml
    
    * Update tests.yml
    
    * fix workflow yaml
    
    * debug
    
    * Update script.sh
    
    * Update action.yml
    
    * Update action.yml
    
    * Update tests.yml
    
    * Update tests.yml
    
    * Update tests.yml

--- script.sh
+++ script.sh
@@ -3,11 +3,14 @@
 # Fail the job if there are any failures below.
 set -e
 
-test -f "package-lock.json" || (echo "Error! Expected a package-lock.json file but none was found." && exit 1)
+# Check if the file exists
+if [ ! -f "$2" ]; then
+    echo "Error! Expected a package-lock.json file at '$2' but none was found."
+    exit 1
+fi
 
-VER=`cat package-lock.json | jq '.lockfileVersion'`
+VER=$(jq '.lockfileVersion' "$2")
 
 if [ "$1" != "$VER" ]; then
     echo "Error! package-lock.json lockfileVersion is $VER but expected $1" && exit 1
 fi
-
