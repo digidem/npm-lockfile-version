b422da9657c3c0f8caf6fce6a9ed6ca7c6b9b842
Gregor MacLennan <gmaclennan@awana.digital>
2025-03-14 13:29:03 +0900

    Use path input to check lockfile

--- script.sh
+++ script.sh
@@ -3,11 +3,10 @@
 # Fail the job if there are any failures below.
 set -e
 
-test -f "package-lock.json" || (echo "Error! Expected a package-lock.json file but none was found." && exit 1)
+test -f "$2" || (echo "Error! Expected a package-lock.json file but none was found." && exit 1)
 
-VER=`cat package-lock.json | jq '.lockfileVersion'`
+VER=`cat "$2" | jq '.lockfileVersion'`
 
 if [ "$1" != "$VER" ]; then
     echo "Error! package-lock.json lockfileVersion is $VER but expected $1" && exit 1
 fi
-
